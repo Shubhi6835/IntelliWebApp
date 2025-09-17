e6e2670c1c1feb8a319da7c6a74cdfddbc08a344
Shubhi6835 <shubhr68350@gmail.com>
2025-09-18 00:44:26 +0530

    sign up and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Login from './pages/Login'
 function App() {
   return (
    <Routes>
-    <Route path='/signup' element={<SignUp/>}/>
+    <Route path='/Signup' element={<SignUp/>}/>
      <Route path='/login' element={<Login/>}/>
    </Routes>
   )
---
+++ frontend/src/context/userData.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import React, { createContext } from 'react'
+export const userDataContext=createContext()
+function UserContext({children}) {
+    const serverUrl="http://localhost:3000"
+    const value={serverUrl}
+    return(
+      <div>
+        <userDataContext.Provider value={value}>
+          {children}
+        </userDataContext.Provider>
+      </div>
+    )
+}
+
+export default UserContext;
+      
